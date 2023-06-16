7ea7649035659c56c0630733ce15fc0204d7d591
Randy Goldsmith <randyg@Randys-MacBook-Pro.local>
2023-06-15 23:43:30 -0500

    finished project

--- error-handling.js
+++ error-handling.js
@@ -1,19 +1,42 @@
 // 1.
 function sum(array) {
   let sum = 0;
-  for (let i = 0; i < array.length; i++) {
-    sum += array[i];
+  try {
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+  } catch(error) {
+    if(error instanceof TypeError) {
+      console.log(`${error.name}: ${error.message}`);
+    } else {
+      throw Error;
+    }
   }
   return sum;
 }
 
-let res = sum(null);
-console.log(res);
+
+  let res = sum(null);
+  console.log(res);
 
 // 2.
+
+function sayName(name) {
+  if(name === String) {
+    return name;
+  } else {
+    throw new TypeError("Invalid name! Must be a string!");
+  }
+}
 // tests
-sayName("Alex");
-sayName(1);
+try{
+  sayName("Alex");
+  sayName(1);
+}
+catch(error) {
+  console.log(`${error.message}`);
+}
+
 // Your code here
 
 // 3.
@@ -23,4 +46,10 @@ function greet(greeting) {
   }
 
   console.log(greeting);
+}
+
+try {
+  greet();
+} catch(error) {
+  console.error(`${error.message}`);
 }
